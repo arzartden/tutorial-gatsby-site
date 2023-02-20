4f34b067724d16e4ab9140802781671872e99cb9
arzartden <arzartden@yandex.ru>
2023-02-20 20:43:18 +0700

    tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `landscapedesigner`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+  ],
 }
