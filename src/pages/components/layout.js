caec8278af189cd62232b8544c8bf9e4d186330e
arzartden <arzartden@yandex.ru>
2023-02-21 10:08:27 +0700

    tutorial part 4

--- src/pages/components/layout.js
+++ src/pages/components/layout.js
@@ -1,20 +1,34 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import {
   container,
+  siteTitle,
   heading,
   navLinks,
   navLinkItem,
   navLinkText,
+  text
 } from './layout.module.css';
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <div className={ container }>
+      <header className={ siteTitle }>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={ navLinks }>
           <li className={ navLinkItem }><Link to='/' className={ navLinkText }>Home</Link></li>
           <li className={ navLinkItem }><Link to='/about' className={ navLinkText }>About</Link></li>
+          <li className={ navLinkItem }><Link to='/blog' className={ navLinkText }>Blog</Link></li>
         </ul>
       </nav>
       <main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import Layout from './components/layout';
+import Seo from './components/seo';
 import { StaticImage } from 'gatsby-plugin-image';
+
+const img = {
+  maxWidth: '200px',
+  maxHeight: '200px',
+};
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
+      <StaticImage style={ img }
         alt="Gatsby logo"
         src="../images/icon.png"
       />
@@ -14,6 +20,6 @@ const IndexPage = () => {
   );
 };
 
-export const Head = () => <title>Home Page</title>;
+export const Head = () => <Seo title='Home Page'/>;
 
 export default IndexPage;
